d08f722a4f2cba0e888b57eada8a20d5ae792953
Igor <igor.shiota@gmail.com>
2025-07-07 12:56:46 +0100

    fixing firebase config to use envinronment variables

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,21 +1,19 @@
-// Import Firebase SDK
+// src/firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
-// Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBn9pOj45XErxMfkZl4JE0D8NGSJi4wscA",
-  authDomain: "booking-app-nuig.firebaseapp.com",
-  projectId: "booking-app-nuig",
-  storageBucket: "booking-app-nuig.appspot.com",
-  messagingSenderId: "924146889227",
-  appId: "1:924146889227:web:a6b4de64f01076930e8d0e",
-  measurementId: "G-40DMB5DZD0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
