1c417ecc03d67c53ec77dd72bc6bb141446b8276
Igor <igor.shiota@gmail.com>
2025-07-07 13:06:50 +0100

    fixing express issues

--- server/index.js
+++ server/index.js
@@ -4,7 +4,10 @@ const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
 
+
 const app = express();
+app.disable('x-powered-by');
+
 const PORT = 8080;
 
 // Log incoming requests to /images
